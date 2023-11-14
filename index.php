1c5a62f9b807307c72db1d0eb012408d4a8d059f
Ashadi Setiawan <ashadi.setiawan@gmail.com>
2023-11-14 16:38:08 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,9 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi ea sit maxime unde velit autem rem atque voluptates fugiat doloremque, quisquam expedita corporis iusto praesentium consequatur non. Autem, eveniet sunt!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
